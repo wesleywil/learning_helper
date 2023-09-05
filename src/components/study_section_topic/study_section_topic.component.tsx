f853a07482d1cb01f6ae13db08dc86509c68dccf
wesleywil <wesleywilsonti@gmail.com>
2023-09-05 13:30:15 -0300

    updated study_section_topic by including a button to hide/show the sub topics container component

--- src/components/study_section_topic/study_section_topic.component.tsx
+++ src/components/study_section_topic/study_section_topic.component.tsx
@@ -1,4 +1,8 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { FaEdit, FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { AppDispatch } from "@/redux/store";
+import { handleHideSubTopicsContainer } from "@/redux/utils/utils";
 
 type StudySectionTopicProps = {
   topic: string;
@@ -12,17 +16,30 @@ const StudySectionTopic = ({
   children,
 }: StudySectionTopicProps) => {
   const [hidden, setHidden] = useState<boolean>(true);
+
+  const dispatch = useDispatch<AppDispatch>();
   return (
-    <div className="w-full px-2 mt-2 text-white bg-black/60 rounded overflow-hidden">
-      <button
-        onClick={() => setHidden(!hidden)}
-        className="text-left text-xl font-bold"
-      >
-        {topic}
-      </button>
+    <div className="w-full mt-2 text-white bg-black/60 rounded overflow-hidden">
+      <div className="flex gap-8">
+        <button
+          onClick={() => dispatch(handleHideSubTopicsContainer())}
+          className="px-2 hover:text-slate-800 bg-slate-800 hover:bg-slate-100 transform duration-500 ease-in-out"
+        >
+          <FaEdit />
+        </button>
+        <button
+          onClick={() => setHidden(!hidden)}
+          className="flex items-center gap-1 text-left text-xl font-bold"
+        >
+          {hidden ? <FaChevronDown /> : <FaChevronUp />}
+
+          {topic}
+        </button>
+      </div>
+
       <div className={`${hidden ? "hidden" : ""} border-t border-white`}>
-        <p>{description}</p>
-        <div>{children}</div>
+        <p className="px-2">{description}</p>
+        <div className="px-2">{children}</div>
       </div>
     </div>
   );
