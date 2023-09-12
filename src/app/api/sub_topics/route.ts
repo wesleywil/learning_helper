d0f998e228b222a9e8647244c325cac6c7a144f5
wesleywil <wesleywilsonti@gmail.com>
2023-09-12 13:51:02 -0300

    updated sub_topics route by adding the GET method

--- src/app/api/sub_topics/route.ts
+++ src/app/api/sub_topics/route.ts
@@ -1,6 +1,20 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
 
+export async function GET(request:Request){
+  try{
+    const subtopics = await prisma.sub_Topic.findMany();
+    return NextResponse.json(subtopics)
+  }catch (error: any) {
+    return NextResponse.json(
+      {
+        error: `An error occurred while trying to fetch the topics, error: ${error.message}`,
+      },
+      { status: 500 }
+    );
+  }
+}
+
 export async function POST(request: Request) {
   const { topicId, title, description, finished } = await request.json();
 
