373bcfe99525565c4b33d3cfd199b9697cf286c7
wesleywil <wesleywilsonti@gmail.com>
2023-09-14 12:32:54 -0300

    updated study_section_topic component by fixing some styles issues

--- src/components/study_section_topic/study_section_topic.component.tsx
+++ src/components/study_section_topic/study_section_topic.component.tsx
@@ -38,24 +38,55 @@ const StudySectionTopic = ({ topic }: StudySectionTopicProps) => {
   };
 
   return (
-    <div className="w-full mt-2 text-[#edf0ef] bg-[#371e30]/60 flex justify-between rounded overflow-hidden">
-      <div className="flex gap-8">
-        <button
-          onClick={() => handleSelectTopic(topic.id!)}
-          className="px-2 bg-[#f6603b] hover:bg-[#371e30] transform duration-500 ease-in-out"
-        >
-          <FaEdit />
-        </button>
-        <button
-          onClick={() => setHidden(!hidden)}
-          className="flex items-center gap-1 text-left text-xl font-bold"
-        >
-          <>
-            {hidden ? <FaChevronDown /> : <FaChevronUp />}
+    <div className="w-full mt-2 text-[#edf0ef] bg-[#371e30]/60 rounded overflow-hidden">
+      <div className="flex justify-between">
+        <div className="flex gap-8">
+          <button
+            onClick={() => handleSelectTopic(topic.id!)}
+            className="px-2 bg-[#f6603b] hover:bg-[#371e30] transform duration-500 ease-in-out"
+          >
+            <FaEdit />
+          </button>
+          <button
+            onClick={() => setHidden(!hidden)}
+            className="flex items-center gap-1 text-left text-xl font-bold"
+          >
+            <>
+              {hidden ? <FaChevronDown /> : <FaChevronUp />}
 
-            {topic.title}
-          </>
-        </button>
+              {topic.title}
+            </>
+          </button>
+        </div>
+        <div className="flex gap-2">
+          <div
+            style={{}}
+            className={`${
+              deleteMsg ? "h-fit" : "h-0"
+            } flex items-center gap-2 text-slate-300 text-xs  overflow-hidden`}
+          >
+            <h1>Are you sure you want to delete this topic?</h1>
+            <button
+              onClick={() => handleDelete(topic.id!)}
+              className="text-[#edf0ef] hover:text-[#f6603b] text-base transform duration-500 ease-in-out"
+            >
+              Yes
+            </button>
+            /
+            <button
+              onClick={() => setDeleteMsg(!deleteMsg)}
+              className="text-[#edf0ef] hover:text-[#f6603b] text-base transform duration-500 ease-in-out"
+            >
+              No
+            </button>
+          </div>
+          <button
+            onClick={() => setDeleteMsg(!deleteMsg)}
+            className="px-2 bg-[#f6603b] hover:bg-[#371e30] transform duration-500 ease-in-out"
+          >
+            <FaTrashAlt />
+          </button>
+        </div>
       </div>
 
       <div className={`${hidden ? "hidden" : ""} border-t border-[#f6603b]`}>
@@ -69,35 +100,6 @@ const StudySectionTopic = ({ topic }: StudySectionTopicProps) => {
                 ))
             : ""}
         </div>
-      </div>
-      <div className="flex gap-2">
-        <div
-          style={{}}
-          className={`${
-            deleteMsg ? "h-fit" : "h-0"
-          } flex items-center gap-2 text-slate-300 text-xs  overflow-hidden`}
-        >
-          <h1>Are you sure you want to delete this topic?</h1>
-          <button
-            onClick={() => handleDelete(topic.id!)}
-            className="text-[#edf0ef] hover:text-[#f6603b] text-base transform duration-500 ease-in-out"
-          >
-            Yes
-          </button>
-          /
-          <button
-            onClick={() => setDeleteMsg(!deleteMsg)}
-            className="text-[#edf0ef] hover:text-[#f6603b] text-base transform duration-500 ease-in-out"
-          >
-            No
-          </button>
-        </div>
-        <button
-          onClick={() => setDeleteMsg(!deleteMsg)}
-          className="px-2 bg-[#f6603b] hover:bg-[#371e30] transform duration-500 ease-in-out"
-        >
-          <FaTrashAlt />
-        </button>
       </div>
     </div>
   );
